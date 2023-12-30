c8e53fba58c6cf99a538ff37b22b965a7c8cd41b
NatKhomich <yalapulya94@mail.ru>
2023-12-30 12:26:23 +0600

    change name index.html

--- src/index.tsx
+++ src/index.tsx
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(
 )
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename={"/task-manager"}>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
 // basename={"/task-manager"}
